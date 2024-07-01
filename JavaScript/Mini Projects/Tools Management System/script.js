ac65d246c8a21f477eecb21290ae7b00ae6c290e
Ajith Kumar <akeditzdj@gmail.com>
2024-07-01 18:07:17 +0800

    load data in table

--- JavaScript/Mini Projects/Tools Management System/script.js
+++ JavaScript/Mini Projects/Tools Management System/script.js
@@ -1,4 +1,36 @@
-mobiscroll.select("#multiple-select", {
-  inputElement: document.getElementById("my-input"),
-  touchUi: false,
-});
+let tools = [
+  {
+    id: 1,
+    date: Date.now(),
+    toolsName: "Impact Wrench",
+    personName: "Ajith",
+    time: Date.now(),
+  },
+  {
+    id: 2,
+    date: Date.now(),
+    toolsName: "I-pad",
+    personName: "Bala",
+    time: Date.now(),
+  },
+];
+
+
+
+function loadData() {
+  let output = "";
+  tools.forEach((tool, index) => {
+    output += `<tr>
+    <td>${index + 1}</td>
+    <td>${tool.date}</td>
+    <td>${tool.toolsName}</td>
+    <td>${tool.personName}</td>
+    <td>${tool.time}</td>
+    </tr>
+    `;
+  });
+
+  document.getElementById("tbody").innerHTML = output;
+}
+
+loadData()
