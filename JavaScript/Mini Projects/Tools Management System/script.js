c9cbab4128049c5eac222fa51ae0e869a585bc27
Ajith Kumar <akeditzdj@gmail.com>
2024-07-01 18:51:46 +0800

    add time function

--- JavaScript/Mini Projects/Tools Management System/script.js
+++ JavaScript/Mini Projects/Tools Management System/script.js
@@ -1,14 +1,14 @@
 let tools = [
   {
     id: 1,
-    date: Date.now(),
+    date: today,
     toolsName: "Impact Wrench",
     personName: "Ajith",
     time: Date.now(),
   },
   {
     id: 2,
-    date: Date.now(),
+    date: today,
     toolsName: "I-pad",
     personName: "Bala",
     time: Date.now(),
@@ -16,13 +16,16 @@ let tools = [
 ];
 
 
+loadData();
+
+console.log(tools);
 
 function loadData() {
   let output = "";
   tools.forEach((tool, index) => {
     output += `<tr>
     <td>${index + 1}</td>
-    <td>${tool.date}</td>
+    <td>${tool.today}</td>
     <td>${tool.toolsName}</td>
     <td>${tool.personName}</td>
     <td>${tool.time}</td>
@@ -33,4 +36,41 @@ function loadData() {
   document.getElementById("tbody").innerHTML = output;
 }
 
-loadData()
+
+
+// get current date
+
+// function currentDate() {
+//     var today = new Date();
+//     var dd = String(today.getDate()).padStart(2, "0");
+//     var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+//     var yyyy = today.getFullYear();
+
+//     today = dd + "/" + mm + "/" + yyyy;
+
+// }
+// currentDate();
+
+  var today = new Date();
+    var dd = String(today.getDate()).padStart(2, "0");
+    var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+    var yyyy = today.getFullYear();
+
+today = dd + "/" + mm + "/" + yyyy;
+
+/*
+
+let scores = {
+  abel: 45.6,
+  brian: 34.6,
+  caroline: 39.8,
+  david: 24.5,
+};
+
+Object.keys(scores).map((name) => (scores[name] += 5));
+
+console.log(scores);
+
+
+{ abel: 50.6, brian: 39.6, caroline: 44.8, david: 29.5 }
+*/
