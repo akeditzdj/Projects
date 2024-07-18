9246059da9282af2daee977dfb3b8187f8201484
Ajith Kumar <akeditzdj@gmail.com>
2024-07-18 21:38:07 +0800

    add css styles

---
+++ JavaScript/Mini Projects/Form Validation/asset/js/script.js
@@ -0,0 +1,14 @@
+const form = document.getElementById("form");
+const emailElement = document.getElementById("email");
+const passwordElement = document.getElementById("password");
+const confirm_PasswordElement = document.getElementById("confirm-password");
+const f_NameElement = document.getElementById("f-name");
+const l_NameElement = document.getElementById("l-name");
+const gederElement = document.querySelectorAll("gender");
+const countryElement = document.getElementById("country");
+const termsElement = document.getElementById("terms");
+const newsLetterElement = document.getElementById("newsletter");
+const btnSubmit = document.getElementById("submit");
+const errorMSg = document.querySelectorAll(".errorMsg");
+
+
