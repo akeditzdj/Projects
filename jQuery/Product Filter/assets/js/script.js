f3caa58effc67253321f10e07ca07713e6cbcda5
Ajith Kumar <akeditzdj@gmail.com>
2024-09-28 16:05:59 +0800

    update filter function

--- jQuery/Product Filter/assets/js/script.js
+++ jQuery/Product Filter/assets/js/script.js
@@ -3,48 +3,40 @@ const ramList = document.getElementById("ramList");
 const internalList = document.getElementById("internalList");
 const showProduct = document.getElementById("showProduct");
 const txtSearch = document.getElementById("searchTxt");
-// const reviewList = document.getElementById("reviewList");
 
 let fetchedProducts = [];
 
+// Fetch products from JSON file
 async function fetchProduct() {
-  let products = [];
   try {
     const url = "assets/js/products.json";
     const response = await fetch(url);
-    const products = await response.json();
-    return products;
+    return await response.json();
   } catch (error) {
     console.log(error);
+    return []; // Return empty array on error
   }
-  return products;
 }
 
-
-
+// Filter categories for dropdown and checkboxes
 function categoryFilter(categoryType, element, products) {
-  // Get unique categories from products
   const uniqueCategories = Array.from(
     new Set(products.map((product) => product[categoryType]))
   );
 
-  // Sort categories based on type
   const sortedData =
     categoryType === "brand"
       ? uniqueCategories.sort()
       : uniqueCategories.map(Number).sort((a, b) => a - b);
 
-  // Clear the element's innerHTML
-  element.innerHTML = "";
+  element.innerHTML = ""; // Clear the element's innerHTML
 
   if (categoryType === "brand") {
-    // Create a dropdown for brands with 'All' option
     const options = sortedData
       .map((item) => `<option value='${item}'>${item}</option>`)
       .join("");
     element.innerHTML = `<option value="All">All</option>${options}`;
   } else {
-    // Create checkboxes for other categories with 'All' option
     const checkboxes =
       `
       <li>
@@ -71,49 +63,48 @@ function categoryFilter(categoryType, element, products) {
   }
 }
 
-
-// Load products
+// Load products into the DOM
 function loadProducts(products) {
   let output = "";
   if (products.length > 0) {
     products.forEach((product) => {
       output += `
-               <div class="card product">
-                         <div class="card-img"><img src="${product.img}" alt="${product.title}" title="${product.title}"></div>
-                         <div class="product-info">
-                        <h2 title="${product.title}">${product.title}</h2>
-                        <div class="star-rating" id="starRating">
-                            <span class="star" data-value="1">&#9733;</span>
-                            <span class="star" data-value="2">&#9733;</span>
-                            <span class="star" data-value="3">&#9733;</span>
-                            <span class="star" data-value="4">&#9733;</span>
-                            <span class="star" data-value="5">&#9733;</span>
-                            <p>Review : ${product.review}</p>
-                        </div>
-                        <div class="price">
-                            <h4 class="old-price">Rs.${product.old_price}/-</h4>
-                            <h4 class="new-price">Rs.${product.new_price}/-</h4>
-                        </div>
-                        <div class="varient">
-                            <p><span>Brand</span> : ${product.brand} </p>
-                            <p><span>Color</span> : ${product.color}</p>
-                        </div>
-                        <div class="storage">
-                            <p><span>RAM</span> : ${product.ram}GB</p>
-                            <p><span>Internal</span> : ${product.internal}GB</p>
-                        </div>
-                        <button class="btn-cart">Add To Cart</button>
-                        </div>
-                    </div>
-`;
-      showProduct.innerHTML = output;
+        <div class="card product">
+          <div class="card-img"><img src="${product.img}" alt="${product.title}" title="${product.title}"></div>
+          <div class="product-info">
+            <h2 title="${product.title}">${product.title}</h2>
+            <div class="star-rating" id="starRating">
+              <span class="star" data-value="1">&#9733;</span>
+              <span class="star" data-value="2">&#9733;</span>
+              <span class="star" data-value="3">&#9733;</span>
+              <span class="star" data-value="4">&#9733;</span>
+              <span class="star" data-value="5">&#9733;</span>
+              <p>Review : ${product.review}</p>
+            </div>
+            <div class="price">
+              <h4 class="old-price">Rs.${product.old_price}/-</h4>
+              <h4 class="new-price">Rs.${product.new_price}/-</h4>
+            </div>
+            <div class="varient">
+              <p><span>Brand</span> : ${product.brand}</p>
+              <p><span>Color</span> : ${product.color}</p>
+            </div>
+            <div class="storage">
+              <p><span>RAM</span> : ${product.ram}GB</p>
+              <p><span>Internal</span> : ${product.internal}GB</p>
+            </div>
+            <button class="btn-cart">Add To Cart</button>
+          </div>
+        </div>
+      `;
     });
+    showProduct.innerHTML = output;
   } else {
     showProduct.innerHTML = "<p>Products Not Found</p>";
   }
 }
 
-// Helper function to get selected options from checkboxes
+// Get selected options from checkboxes
 function getSelectedOptions(listSelector, dataAttribute) {
   return [...listSelector.querySelectorAll("input[type=checkbox]:checked")]
     .map((checkbox) => checkbox.getAttribute(dataAttribute))
@@ -133,10 +124,8 @@ function filterProducts() {
   }
 
   // Filter by brand
-
   const selectedBrands = brandList.value;
-
-  if (selectedBrands) {
+  if (selectedBrands && selectedBrands !== "All") {
     filteredProducts = filteredProducts.filter((product) =>
       selectedBrands.includes(product.brand)
     );
@@ -162,37 +151,35 @@ function filterProducts() {
   loadProducts(filteredProducts);
 }
 
-// Set up filter event listeners
+// Set up event listeners for filters
 function setUpEventListeners() {
-  // Search input event listener
   txtSearch.addEventListener("input", filterProducts);
 
-  // Brand, RAM, Internal storage filter event listeners
-  brandList.addEventListener("change", filterProducts);
-  ramList.addEventListener("change", filterProducts);
-  internalList.addEventListener("change", filterProducts);
+  [brandList, ramList, internalList].forEach((list) => {
+    list.addEventListener("change", filterProducts);
+  });
 }
 
-// Update price function when you drag the price range
-
+// Update price display when price range is adjusted
 function updateTextInput(val) {
   document.getElementById("priceValue").innerHTML = "Rs." + val;
 }
 
+// Toggle between Grid and List view
 document.getElementById("list").addEventListener("click", function () {
   let products = document.querySelectorAll(".product");
   products.forEach(function (product) {
-    // product.classList.remove("card"); // No dot here
     product.classList.add("card-list");
   });
 });
+
 document.getElementById("grid").addEventListener("click", function () {
   let products = document.querySelectorAll(".product");
   products.forEach(function (product) {
-    product.classList.add("card"); // No dot here
     product.classList.remove("card-list");
   });
 });
+
 // Initialize the app
 window.addEventListener("load", async function () {
   fetchedProducts = await fetchProduct();
