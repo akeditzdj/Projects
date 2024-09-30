3474fedb59b049265abd4322a94046c0d6f5c3c8
Ajith Kumar <akeditzdj@gmail.com>
2024-09-30 23:47:46 +0800

    change css style and responsive design

--- React/Project-1/vite-project/src/components/card/Card.jsx
+++ React/Project-1/vite-project/src/components/card/Card.jsx
@@ -32,26 +32,7 @@ let userData = [
     name: "John Doe",
     status: "2d ago",
   },
-  {
-    cardImg: "/images/car.jpg",
-    category: "Technology",
-    title: "Race To Your Heart Content",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi perferendis molestiae non nemo doloribus. Doloremque, nihil! At ea atque quidem!",
-    profileImg: "/images/robo.webp",
-    name: "John Doe",
-    status: "2d ago",
-  },
-  {
-    cardImg: "/images/food.jpg",
-    category: "Food",
-    title: "Delicious Food",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi perferendis molestiae non nemo doloribus. Doloremque, nihil! At ea atque quidem!",
-    profileImg: "/images/baby.jpg",
-    name: "Jony Doe",
-    status: "Yesterday",
-  },
+
 ];
 function Card(props) {
   return (
@@ -60,14 +41,24 @@ function Card(props) {
         <img className="img-fluid" src={props.cardImg} alt="Laptop" />
       </div>
       <div className="card-body p-3">
-        <p className={props.category == "Technology"? "tag technology": props.category == "Food"? "tag food": "tag automobile"}>{props.category}</p>
+        <p
+          className={
+            props.category == "Technology"
+              ? "tag technology"
+              : props.category == "Food"
+              ? "tag food"
+              : "tag automobile"
+          }
+        >
+          {props.category}
+        </p>
         <h5 className="mt-3">{props.title}</h5>
         <p className="mt-2 text-secondary">{props.description}</p>
         <div className="profile d-flex justify-content-start align-items-center mt-4">
           <div className="profile-img">
             <img className="image-fluid" src={props.profileImg} alt="Girl" />
           </div>
-          <div className="profile-info mt-2">
+          <div className="profile-info mt-3 ps-2">
             <h6 className="m-0">{props.name}</h6>
             <p className="text-secondary">{props.status}</p>
           </div>
@@ -95,6 +86,8 @@ export const UserCard = () => {
     </>
   );
 };
+
+// PropsType
 
 Card.propTypes = {
   cardImg: PropTypes.string.isRequired,
--- React/Project-1/vite-project/src/main.jsx
+++ React/Project-1/vite-project/src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { UserCard } from "./components/card/Card";
-// import App from './App.jsx'
-// import "./index.css";
+// import App from "./App.jsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
