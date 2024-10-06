67d28a7fcd9bb648d865e0fb7e7319f99ddf64e8
Ajith Kumar <akeditzdj@gmail.com>
2024-10-06 11:20:27 +0800

    change index page title

--- React/Shop_Cart/src/Components/Product Container/ProductList.jsx
+++ React/Shop_Cart/src/Components/Product Container/ProductList.jsx
@@ -1,9 +1,15 @@
+import products from "./fooditems.json";
+
 const ProductList = () => {
-    return <>
-        <div className="container product-container">
-              <h4 className="mt-3">Product List</h4>
-        </div>
-    </>;
+  return (
+    <>
+      <div className="container product-container">
+        <h4 className="mt-3">Product List</h4>
+        {products &&
+          products.map((product) => <ProductList product={product} key={product.id} />)}
+      </div>
+    </>
+  );
 };
 
 export default ProductList;
