5835028a855094f1b441f9f13e252b3d9d569a61
Ajith Kumar <akeditzdj@gmail.com>
2024-10-07 13:48:53 +0800

    change cart items style

--- React/Shop_Cart/src/Components/Cart Items/CartItems.jsx
+++ React/Shop_Cart/src/Components/Cart Items/CartItems.jsx
@@ -6,21 +6,26 @@ const CartItems = ({ cartItems }) => {
       {cartItems.map((item) => (
         <div key={item.id} className="p-2 cart-list-container">
           <div className="cart-list d-flex justify-content-between align-items-center p-2">
-            <div className="cart-img">
-              <img src={item.img} alt={item.name} />
+            <div className="d-md-flex justify-content-start align-items-center gap-2">
+              <div className="cart-img">
+                <img src={item.img} alt={item.name} />
+              </div>
+              <h5 className="text-truncate">{item.name}</h5>
             </div>
-            <h5 className="text-truncate">{item.name}</h5>
-            <div className="cart-price">Rs.{item.offer_price}</div>
-            <div>
-              <input
-                className="form-control form-control-sm"
-                type="number"
-                id="qty"
-              />
+            <div className="d-flex flex-wrap justify-content-end align-items-center gap-2">
+              <div className="cart-price">Rs.{item.offer_price}</div>
+              <div>
+                <input
+                  className="form-control form-control-sm"
+                  type="number"
+                  value="1"
+                  id="qty"
+                />
+              </div>
+              <button className="btn btn-sm btn-danger">
+                <i className="bi bi-trash-fill"></i>
+              </button>
             </div>
-            <button className="btn btn-sm btn-danger">
-              <i className="bi bi-trash-fill"></i>
-            </button>
           </div>
         </div>
       ))}
