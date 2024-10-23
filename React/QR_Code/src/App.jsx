ccaedd1c32028fca8a1f4d80dab243a1231ab7e3
Ajith Kumar <akeditzdj@gmail.com>
2024-10-23 19:47:07 +0800

    create new project

---
+++ React/QR_Code/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="container border">
+        <h2>QR Code Generator</h2>
+        <div className="form-group">
+          <input className="form-control" type="text" />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
