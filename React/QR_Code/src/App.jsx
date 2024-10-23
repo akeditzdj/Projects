2a3885ff4f20a7f52e590cab8138e6dc66d48726
Ajith Kumar <akeditzdj@gmail.com>
2024-10-23 22:24:50 +0800

    add qr code photo

--- React/QR_Code/src/App.jsx
+++ React/QR_Code/src/App.jsx
@@ -1,19 +1,50 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-
-
   return (
     <>
-      <div className="container border">
-        <h2>QR Code Generator</h2>
-        <div className="form-group">
-          <input className="form-control" type="text" />
+      <div className="container">
+        <h3 className="mb-2 text-center text-primary">QR Code Generator</h3>
+        <img className="img-fluid" src="QRCode.png" alt="QRCode" />
+        <div className="my-3">
+          <label className="form-label" htmlFor="dataInput">
+            Data for QR Code
+          </label>
+          <input
+            className="form-control"
+            id="dataInput"
+            type="text"
+            placeholder="Enter Data for QR Code"
+          />
         </div>
+        <div className="my-3">
+          <label className="form-label" htmlFor="imageSize">
+            Image Size (e.g., 150):
+          </label>
+          <input
+            className="form-control"
+            id="imageSize"
+            type="text"
+            placeholder="Enter image size"
+          />
+        </div>
+        <div className="my-4 d-flex justify-content-center align-items-center gap-3">
+          <button className="btn btn-primary">Generate QR Code</button>
+          <button className="btn btn-danger">Download QR Code</button>
+        </div>
+        <footer className="mt-3 text-center">
+          <p className="text-danger">Designed By</p>
+          <a
+            className="link-primary text-decoration-none"
+            target="_blank"
+            href="https://www.akeditz.com"
+          >
+            Ak Editz Design and Develop
+          </a>
+        </footer>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
