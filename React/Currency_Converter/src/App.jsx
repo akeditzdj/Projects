356e30c0772816dfebdd615e6e0a005340e5d47f
Ajith Kumar <akeditzdj@gmail.com>
2024-10-27 19:50:41 +0800

    get data from exchange rate API

--- React/Currency_Converter/src/App.jsx
+++ React/Currency_Converter/src/App.jsx
@@ -1,8 +1,31 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import banner from "./assets/cc.jpeg";
+import axios from "axios";
 
 function App() {
+  const [amount, setAmount] = useState("1");
+  const [fromCurrency, setFromCurency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("INR");
+  const [convertedAmount, setConvertedAmount] = useState(null);
+
+  useEffect(() => {
+    const getExchangeRate = async () => {
+      try {
+        let url = `https://api.exchangerate-api.com/v4/latest/${fromCurrency}`;
+        const response = await axios.get(url);
+        console.log(response);
+      } catch (error) {
+        console.error("Error fetching exchange rate:", error);
+      }
+    };
+  });
+
+  const handleAmountChange = (e) => {
+    const value = parseFloat(e.target.value);
+    setAmount(isNaN(value) ? 0 : value);
+  };
+
   return (
     <>
       <div className="app-container">
@@ -13,11 +36,17 @@ function App() {
           <h1>Currency Converter</h1>
           <div className="input-group">
             <label htmlFor="amt">Amount:</label>
-            <input type="text" id="amt" placeholder="Enter your amount value" />
+            <input
+              type="text"
+              id="amt"
+              placeholder="Enter your amount value"
+              value={amount}
+              onChange={handleAmountChange}
+            />
           </div>
           <div className="input-group">
             <label htmlFor="fromCurrency">From Currency:</label>
-            <select id="fromCurrency">
+            <select id="fromCurrency" value={fromCurrency}>
               <option value="USD">United States Dollar (USD)</option>
               <option value="EUR">Euro (EUR)</option>
               <option value="JPY">Japanese Yen (JPY)</option>
@@ -32,7 +61,7 @@ function App() {
           </div>
           <div className="input-group">
             <label htmlFor="toCurrency">To Currency:</label>
-            <select id="toCurrency">
+            <select id="toCurrency" value={toCurrency}>
               <option value="USD">United States Dollar (USD)</option>
               <option value="EUR">Euro (EUR)</option>
               <option value="JPY">Japanese Yen (JPY)</option>
@@ -46,10 +75,14 @@ function App() {
             </select>
           </div>
           <div className="result">
-            <p>1 INR is equal to 83.25 USD</p>
+            <p>
+              {amount} {fromCurrency} is equal to {convertedAmount} {toCurrency}
+            </p>
           </div>
           <div className="footer">
-            <p>&copy;Copyright <a href="https://akeditz.com">AK Editz</a></p>
+            <p>
+              &copy;Copyright <a href="https://akeditz.com">AK Editz</a>
+            </p>
           </div>
         </div>
       </div>
