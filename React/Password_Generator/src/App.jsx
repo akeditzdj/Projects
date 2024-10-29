6d12d6ccbd4d1e757600b00e15354e1d3c50dcec
Ajith Kumar <akeditzdj@gmail.com>
2024-10-29 17:08:40 +0800

    add change event

--- React/Password_Generator/src/App.jsx
+++ React/Password_Generator/src/App.jsx
@@ -2,6 +2,13 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [length, setLenght] = useState(8);
+  const [IncludesUppercase, setIncludesUppercase] = useState(true);
+  const [IncludesLowercase, setIncludesLowercase] = useState(true);
+  const [IncludesNumbers, setIncludesNumbers] = useState(true);
+  const [ IncludesSymbols, setIncludesSymbols ] = useState( true );
+  const [password,setPassword]=useState("")
+
   const btnCopy = (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -19,27 +26,35 @@ function App() {
         <h1>Password Generator</h1>
         <div className="input-group">
           <label htmlFor="pass-len">Choose Password length:</label>
-          <input type="number" id="pass-len" min="0" max="16" />
+          <input
+            type="number"
+            id="pass-len"
+            min="0"
+            max="16"
+            value={length}
+            onChange={(e) => setLenght(parseInt(e.target.value))}
+          />
+        </div>
+
+        <div className="check-box-group">
+          <input type="checkbox" name="uppercase" id="uppercase" checked={IncludesUppercase} onChange={(e)=> setIncludesUppercase(e.target.checked)} />
+          <label htmlFor="uppercase">Includes Uppercase</label>
         </div>
         <div className="check-box-group">
-          <input type="checkbox" name="number" id="number" />
-          <label htmlFor="number">Numbers</label>
+          <input type="checkbox" name="lowercase" id="lowercase" checked={IncludesLowercase} onChange={(e)=> setIncludesLowercase(e.target.checked) />
+          <label htmlFor="lowercase">Includes Lowercase</label>
         </div>
         <div className="check-box-group">
-          <input type="checkbox" name="uppercase" id="uppercase" />
-          <label htmlFor="uppercase">Uppercase</label>
+          <input type="checkbox" name="number" id="number" checked={IncludesNumbers} onChange={(e)=> setIncludesNumbers(e.target.checked) />
+          <label htmlFor="number">Includes Numbers</label>
         </div>
         <div className="check-box-group">
-          <input type="checkbox" name="lowercase" id="lowercase" />
-          <label htmlFor="lowercase">Lowercase</label>
-        </div>
-        <div className="check-box-group">
-          <input type="checkbox" name="symbol" id="symbol" />
-          <label htmlFor="symbol">Symbols</label>
+          <input type="checkbox" name="symbol" id="symbol" checked={ IncludesSymbols} onChange={(e)=> setIncludesSymbols(e.target.checked) />
+          <label htmlFor="symbol">Includes Symbols</label>
         </div>
         <div className="result">
           <p>
-            Password is: <span>Ajith195@</span>
+            Password is: <span>{ password}@</span>
           </p>
           <p title="Copy">{btnCopy}</p>
         </div>
