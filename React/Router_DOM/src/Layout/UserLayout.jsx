bc452cbf693148061b1ab1d87daeb5bfb5fcdec5
Ajith Kumar <akeditzdj@gmail.com>
2024-11-09 21:51:02 +0800

    create user details loader

---
+++ React/Router_DOM/src/Layout/UserLayout.jsx
@@ -0,0 +1,14 @@
+const UserLayout = () => {
+  return (
+    <div>
+      <h1>List of Users</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+        voluptatibus quia.
+      </p>
+     
+    </div>
+  );
+};
+
+export default UserLayout;
