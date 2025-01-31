7555ac295a9b468a683d32c85be677441692f638
Ajith Kumar <akeditzdj@gmail.com>
2025-01-31 13:53:06 +0800

    Add pagination

--- React/ReactQuery/src/App.jsx
+++ React/ReactQuery/src/App.jsx
@@ -4,6 +4,7 @@ import RegularFetch from "./Components/RegularFetch";
 import ReactQueryFetch from "./Components/ReactQueryFetch";
 import Home from "./Components/Home";
 import ReactQueryFetchDetail from "./Components/ReactQueryFetchDetail";
+import Pagination from "./Components/Pagination";
 
 function App() {
   return (
@@ -12,12 +13,14 @@ function App() {
         <NavLink to="/">Home</NavLink>
         <NavLink to="/regular">Regular Fetch</NavLink>
         <NavLink to="react-query">React Query Fetch</NavLink>
+        <NavLink to="/react-query-pagination">Pagination</NavLink>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/regular" element={<RegularFetch />} />
         <Route path="/react-query" element={<ReactQueryFetch />} />
         <Route path="/react-query/:id" element={<ReactQueryFetchDetail />} />
+        <Route path="/react-query-pagination" element={<Pagination />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ React/ReactQuery/src/Components/Pagination.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import axios from "axios";
+import { useState } from "react";
+import { useQuery, keepPreviousData } from "@tanstack/react-query";
+
+const fetchItems = (page) => {
+  return axios.get(`http://localhost:3001/items/?_limit=10&_page=${page}`);
+};
+
+const Pagination = () => {
+  const [page, setPage] = useState(1);
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["items", page],
+    queryFn: () => fetchItems(page),
+    placehoderData: keepPreviousData,
+  });
+
+  if (isLoading) {
+    return <p>Please wait while data is loading...</p>;
+  }
+
+  if (isError) {
+    return <p>Error in fetching data {error.message}</p>;
+  }
+
+  return (
+    <div>
+      <ul>
+        {data?.data.map((item) => (
+          <li key={item.id}>
+            {item.id}. {item.name}
+          </li>
+        ))}
+      </ul>
+      <button
+        onClick={() => {
+          setPage((prev) => prev - 1);
+        }}
+        disabled={page == 1 ? true : false}
+      >
+        Previous
+      </button>
+      <button
+        onClick={() => {
+          setPage((prev) => prev + 1);
+        }}
+        disabled={page == 10 ? true : false}
+      >
+        Next
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
